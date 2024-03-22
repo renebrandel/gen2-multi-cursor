02ccff52a39587c98eeb55f4f4713fffa020a2e8
Rene Brandel <renbran@amazon.com>
2024-03-21 17:03:03 -0700

    improved responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ const colors: Record<string, string> = {}
 
 function App() {
   const [cursors, setCursors] = useState<Record<string, { x: number, y: number }>>({})
+  const [myPosition, setMyPosition] = useState<{x: number, y: number}>({x: 0, y: 0})
   const [username, setUsername] = useState<string>(uniqueNamesGenerator({
     dictionaries: [adjectives, animals],
     separator: '-',
@@ -55,6 +56,7 @@ function App() {
     function handleMouseMove(e: MouseEvent) {
       const x = Math.round(window.innerWidth / 2 - e.clientX)
       const y = Math.round(window.innerHeight / 2 - e.clientY)
+      setMyPosition({ x, y })
       debouncedPublish(username, x, y)
     }
 
@@ -72,6 +74,12 @@ function App() {
               x={cursors[username].x}
               y={cursors[username].y}
               key={username} />)}
+          <Cursor
+            username={username}
+            x={myPosition.x}
+            y={myPosition.y}
+            myself
+            key={username} />
       </div>
       <div className='username-container'>
         <div className="username-panel">
@@ -86,11 +94,12 @@ function App() {
   )
 }
 
-function Cursor({ username, x, y }: { username: string, x: number, y: number }) {
+function Cursor({ username, x, y, myself }: { username: string, x: number, y: number, myself?: boolean }) {
   return <div className='cursor' style={{
     background: colors[username],
     left: window.innerWidth / 2 - x,
     top: window.innerHeight / 2 - y,
+    transition: myself ? '' : 'all .35s ease-out',
   }}>{username}</div>
 }
 
